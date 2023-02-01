7af31394b02f426a582bad429cf91a07ea084fcc
Maryam-2222 <107848037+Maryam-2222@users.noreply.github.com>
2023-02-01 14:16:51 +0500

    Add files via upload

---
+++ quiz.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define SIZE 30
+
+int main()
+{	
+	int array[max];
+	int i=0,f=0,min=0;
+	for (i=0; i<5; i++){
+	printf("Enter  array value at \n array[%d],i");
+	scanf("%d",$array[i];
+	
+	
+	
+	}
+	
+	for(f=0;f<5;f++){
+		for(i=0;i<5;i++{
+		if(array[f]>array[i])
+		{
+			minh=array[i]
+			
+			printf("reached");
+		}
+		}
+	}
+	return 0;
+}
+	
+	
+	
+	
+	
+	
